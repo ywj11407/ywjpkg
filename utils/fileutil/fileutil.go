3deb9c110e6c2e447e55f6a8bc95126455b93229
wenzi <ywj11407>
2022-02-14 23:38:26 +0800

    添加文件操作工具

---
+++ utils/fileutil/fileutil.go
@@ -0,0 +1,41 @@
+package fileutil
+
+import (
+	"io"
+	"io/ioutil"
+	"os"
+)
+
+func CopyFile(src string, dst string) error {
+	// Read all content of src to data
+	data, err := ioutil.ReadFile(src)
+	if err != nil {
+		return err
+	}
+	// Write data to dst
+	err = ioutil.WriteFile(dst, data, 0644)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func Copy(src, dst string) error {
+	in, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer in.Close()
+
+	out, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, in)
+	if err != nil {
+		return err
+	}
+	return out.Close()
+}
